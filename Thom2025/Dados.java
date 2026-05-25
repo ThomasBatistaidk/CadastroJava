fddaf92741c4bf0ce525fad488ac984e51b3e8e5
Thomas <amanda.batista.silva23@escola.pr.gov.br>
2026-05-25 11:13:11 -0300

    Add files via upload

---
+++ Thom2025/Dados.java
@@ -0,0 +1,12 @@
+public class Dados {
+        public static void main(String[] args) {
+            String nome = "Thomas";
+            int idade = 16;
+            double altura = 1.60;
+
+            System.out.println(nome);
+            System.out.println(idade);
+            System.out.println(altura);
+        }
+}
+
