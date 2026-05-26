e7fe4c11f8bc85f6e5b65c4ecd8f5ebeb8f0faa6
Thomas <amanda.batista.silva23@escola.pr.gov.br>
2026-05-26 11:52:51 -0300

    Add files via upload

---
+++ CadastroJava-main/CadastroJava-main/Thom2025/Criar.java
@@ -0,0 +1,12 @@
+public class Criar {
+    public static void main(String[] args) {
+        double n1, n2, n3, n4, media;
+        n1 = 60.3;
+        n2 = 75.8;
+        n3 = 66.7;
+        n4 = 81.1;
+        media = (n1 + n2 + n3 + n4) / 4;
+        System.out.println(media);
+        System.out.printf("%.2f", media);
+    }
+}
